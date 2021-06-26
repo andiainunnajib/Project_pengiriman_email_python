b48d89e1a5e169c03a70198208ec47fb29423098
andiainunnajib <andiainunajib59@gmail.com>
2021-06-27 05:56:19 +0700

    project pengiriman email

---
+++ project_basic.py
@@ -0,0 +1,36 @@
+# import necessary packages
+ 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+ 
+# create message object instance
+msg = MIMEMultipart()
+ 
+ 
+message = "Thank you"
+ 
+# setup the parameters of the message
+password = "b@koCil123"
+msg['From'] = "andiainunnajibb@gmail.com"
+msg['To'] = "andiainunnajib59@gmail.com"
+msg['Subject'] = "Subscription"
+ 
+# add in the message body
+msg.attach(MIMEText(message, 'plain'))
+ 
+#create server
+server = smtplib.SMTP('smtp.gmail.com: 587')
+ 
+server.starttls()
+ 
+# Login Credentials for sending the mail
+server.login(msg['From'], password)
+ 
+ 
+# send the message via the server.
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+ 
+server.quit()
+ 
+print ("successfully sent email to", +msg['To'])
