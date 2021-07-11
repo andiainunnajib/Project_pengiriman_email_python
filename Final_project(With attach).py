4af99e59eced6bf333d93e952b639fd80bec74fc
andiainunnajib <andiainunnajib59@gmail.com>
2021-07-11 16:10:35 +0700

    project pengiriman file ke email pyhton

--- Final_project(With attach).py
+++ Final_project(With attach).py
@@ -1,40 +1,51 @@
+import email
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-email_user = 'akukamubisa12@gmail.com'
-email_password = '======='
-email_send = 'andiainunnajibb@gmail.com'
 
-# email_send = open('datasets/receiver_list.txt', 'r')
+
+def get_email(email_send):
+    receiver_list = []
+    with open(email_send, 'r') as email_kirim:
+        for kirim in email_kirim:
+            kirim = kirim.replace('')
+            receiver_list.append(kirim)
+    return receiver_list
 
 subject = 'subject'
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_send
-msg['Subject'] = subject
 
-body = 'INI ADA PROJECT PYTHON MENGIRIM EMAIL LEWAT PYTHON DENGAN ATTCHEMENT FILE!'
+email_user = 'akukamubisa12@gmail.com'
+email_password = 'b@koCil123'
+email_send = 'datasets\receiver_list.txt'
 
-msg.attach(MIMEText(body,'plain'))
+for email1 in email_send:
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email1
+    msg['Subject'] = subject
 
-filename='datasets/KISI-KISI UN SMP 2019.pdf'
-attachment  =open(filename,'rb')
+    body = 'INI ADA PROJECT PYTHON MENGIRIM EMAIL LEWAT PYTHON DENGAN ATTCHEMENT FILE!'
 
-part = MIMEBase('application','octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition',"attachment; filename= "+filename)
+    msg.attach(MIMEText(body,'plain'))
 
-msg.attach(part)
-text = msg.as_string()
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(email_user,email_password)
+    filename='datasets/KISI-KISI UN SMP 2019.pdf'
+    attachment  =open(filename,'rb')
+
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email_user,email_password)
 
 
-server.sendmail(email_user,email_send,text)
-server.quit()
+    server.sendmail(email_user,email1,text)
+    server.quit()
